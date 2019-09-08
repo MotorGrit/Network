0c8cde5757c8c8ff513b5fb6c0c0105bd762cb87
ControlledChaos <greg@ccdzine.com>
2019-09-07 20:58:56 -0800

    Changed default theme to mg-theme.

--- wp-includes/default-constants.php
+++ wp-includes/default-constants.php
@@ -357,7 +357,8 @@ function wp_templating_constants() {
 	 * @since 3.0.0
 	 * @see WP_Theme::get_core_default_theme()
 	 */
-	if ( !defined('WP_DEFAULT_THEME') )
-		define( 'WP_DEFAULT_THEME', 'twentyseventeen' );
+	if ( ! defined( 'WP_DEFAULT_THEME' ) ) {
+		define( 'WP_DEFAULT_THEME', 'mg-theme' );
+	}
 
 }
